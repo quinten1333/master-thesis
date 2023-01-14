225b96f72c94fa42c23edafdecdd3497254f9c24
Quinten Coltof <quintencoltof1@gmail.com>
2023-01-14 22:05:36 +0100

    Feat: First working example

---
+++ exampleConfig.js
@@ -0,0 +1,78 @@
+const exampleConfigPlus = [ // x + 2 - 10
+  {
+    endpoint: 'amqp://rabbitmq',
+    queues: {
+      plusCalculation: {
+        steps: {
+          0: {
+            fnName: 'plus',
+            extraArgs: [2],
+            outId: 0,
+            outQueue: 'minCalculation'
+          },
+        }
+      }
+    }
+  }
+]
+
+const exampleConfigMin = [ // x + 2 - 10
+  {
+    endpoint: 'amqp://rabbitmq',
+    queues: {
+      minCalculation: {
+        steps: {
+          1: {
+            fnName: 'min',
+            extraArgs: [10],
+            outId: 0,
+            outQueue: '!gateway'
+          },
+        }
+      }
+    }
+  }
+]
+
+const exampleConfigGateway = [
+  {
+    endpoint: 'amqp://rabbitmq',
+    queues: {
+      '!gateway': {
+        steps: {
+          2: {
+            fnName: 'reply',
+          }
+        }
+      }
+    }
+  }
+]
+
+const exampleReplyGateway = [
+  {
+    endpoint: 'amqp://rabbitmq',
+    queues: {
+      '!gateway': {
+        steps: {
+          0: {
+            fnName: 'identity',
+            extraArgs: [],
+            outId: 0,
+            outQueue: '!gateway'
+          },
+          1: {
+            fnName: 'reply',
+            extraArgs: [],
+          },
+        }
+      }
+    }
+  }
+];
+
+const exampleData = {
+  reqId: 0,
+  step: 1,
+  input: 10, // Whatever type of object
+}
