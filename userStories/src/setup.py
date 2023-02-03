ec7ce8f409b999161f0215529ada9c02bbb686ca
Quinten Coltof <quintencoltof1@gmail.com>
2023-02-03 10:46:52 +0100

    Feat(userStories): setup script

---
+++ userStories/src/setup.py
@@ -0,0 +1,8 @@
+import nltk
+
+nltk.download("stopwords")
+nltk.download("punkt")
+nltk.download("averaged_perceptron_tagger")
+nltk.download("tagsets")
+nltk.download('wordnet')
+nltk.download('omw-1.4')
