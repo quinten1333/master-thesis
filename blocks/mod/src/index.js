8079fd283ced0bf02164e144a0b682ce6b462ff8
Quinten Coltof <quintencoltof1@gmail.com>
2023-02-15 17:40:34 +0100

    Chore(blocks): Cleanup blocks with duplicate functionality

--- blocks/mod/src/index.js
+++
@@ -1,6 +0,0 @@
-import MSAMessaging from '@amicopo/msamessaging';
-
-const io = new MSAMessaging();
-io.register('mod', ({ input: a }, b) => a % b);
-
-io.start();
--- blocks/plus/src/index.js
+++
@@ -1,6 +0,0 @@
-import MSAMessaging from '@amicopo/msamessaging';
-
-const io = new MSAMessaging();
-io.register('plus', ({ input: a }, b) => a + b);
-
-io.start();
