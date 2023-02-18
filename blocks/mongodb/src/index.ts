096e661d3e3a0020258cccb57b8d0186a090d833
Quinten Coltof <quintencoltof1@gmail.com>
2023-02-18 22:50:24 +0100

    Feat(blocks/mongodb): MongoDB query service

---
+++ blocks/mongodb/src/index.ts
@@ -0,0 +1,32 @@
+import MSAMessaging from '@amicopo/msamessaging';
+import { MongoClient } from 'mongodb'
+
+const io = new MSAMessaging();
+
+const connect = async (args: { url: string, db: string, collection: string }) => {
+  const client = new MongoClient(args.url);
+  await client.connect();
+
+  const db = client.db(args.db);
+  const col = db.collection(args.collection);
+
+  return { client, db, col };
+}
+
+
+io.register('query', async ({ input }, args: { condition: any, url: string, db: string, collection: string, one: boolean }) => {
+  const { client, col } = await connect(args);
+
+  let res: any;
+  if (args.one) {
+    res = await col.findOne(input);
+  } else {
+    res = await col.find(input).toArray();
+  }
+
+  await client.close();
+
+  return res;
+})
+
+io.start();
