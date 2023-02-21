57f2b149996a39bd1113b84ac1394316b7537203
Quinten Coltof <quintencoltof1@gmail.com>
2023-02-21 22:12:08 +0100

    Chore: Update testing environment

--- userStories/test.sh
+++ userStories/test.sh
@@ -2,11 +2,15 @@ echo "reboot" > ../libs/msamessaging/reboot.js
 
 python -m src thesisfairPlatform.yml $@
 
-curl -X POST --data-urlencode "yaml=$(cat thesisfairPlatform.yml-compiled.yml | sed "s/\"/'/g")" localhost:3001/architecture
+if [ $1 ]; then
+  exit
+fi
+
+curl -X POST --data-urlencode "yaml=$(cat compiled.yml | sed "s/\"/'/g")" localhost:3001/architecture
 echo
 curl localhost:3001/architecture/0/active?active=true
 echo
 
 sleep 0.1
-curl http://localhost:3000/user/login?username=quinten
+curl "http://localhost:3000/user/login?username=quinten&password=pwd"
 echo
