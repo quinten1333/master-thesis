667150bbe600555f5ac06d9750892bac3a3eb4f5
Quinten Coltof <quintencoltof1@gmail.com>
2023-02-22 12:03:09 +0100

    Feat(blocks/architectureManager): React frontend scaffolding

---
+++ blocks/architectureManager/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        Hi There!
+      </div>
+    );
+  }
+}
+
+export default App;
