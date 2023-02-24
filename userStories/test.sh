b11f23c8f17a8aa33a3c11a53dff2705be840540
Quinten Coltof <quintencoltof1@gmail.com>
2023-02-24 11:50:41 +0100

    Fix(userStories): Make test script work again

--- userStories/test.sh
+++ userStories/test.sh
@@ -6,9 +6,9 @@ if [ $1 ]; then
   exit
 fi
 
-curl -X POST --data-urlencode "yaml=$(cat compiled.yml | sed "s/\"/'/g")" localhost:3001/api/architecture
+curl -X POST --data-urlencode "yaml=$(cat compiled.yml | sed "s/\"/'/g")" http://localhost:3001/api/architecture
 echo
-curl localhost:3001/architecture/0/active?active=true
+curl -X PATCH -H 'Content-Type: application/json' -d '{"state": 1}' http://localhost:3001/api/architecture/0
 echo
 
 sleep 0.1
