bfe3dd7a1518f861e0fad285af096ae5c91f5048
Quinten Coltof <quintencoltof1@gmail.com>
2023-03-09 20:37:36 +0100

    Feat(blocks/userStoryCompiler): Unsure only one compilation is happening at a time

--- blocks/userStoryCompiler/src/server.py
+++ blocks/userStoryCompiler/src/server.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import yaml
+import traceback
 
 import compiler
 import drawer
@@ -10,16 +11,38 @@ app = FastAPI()
 class CompileBody(BaseModel):
   yaml: str
 
+lock = False
+def lockFn(func):
+  def wrapper(*args, **kwargs):
+    global lock
+    if lock:
+      return { 'error': 'Other compilation in process' }
+
+    try:
+      res = func(*args, **kwargs)
+    except BaseException as e:
+      lock = False
+      raise e
+
+    lock = False
+    return res
+
+  return wrapper
+
 @app.post("/compile")
 def getCompiled(body: CompileBody):
-  if not body.yaml:
-    return { 'error': 'Missing yaml parameter' }
+  @lockFn
+  def lockedFn():
+    if not body.yaml:
+      return { 'error': 'Missing yaml parameter' }
 
-  doc = yaml.safe_load(body.yaml)
-  try:
-    return { 'data': yaml.dump(compiler.main(doc)) }
-  except BaseException as e:
-    return { 'error': str(e) }
+    doc = yaml.safe_load(body.yaml)
+    try:
+      return { 'data': yaml.dump(compiler.main(doc)) }
+    except BaseException as e:
+      return { 'error': traceback.format_exc() }
+
+  return lockedFn()
 
 @app.post('/draw')
 def getDrawing():
