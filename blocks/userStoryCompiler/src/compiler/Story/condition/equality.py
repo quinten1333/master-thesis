16b991d6d5b64daed3de7b3bb4aab614aad85abd
Quinten Coltof <quintencoltof1@gmail.com>
2023-06-13 15:37:52 +0000

    Chore(userStoryCompiler/story/condition): Rename state to context

--- blocks/userStoryCompiler/src/compiler/Story/condition/equality.py
+++ blocks/userStoryCompiler/src/compiler/Story/condition/equality.py
@@ -1,17 +1,16 @@
-import re
 from ..Story import Story, resolveIdentifierOrObj
 
 def genCompareFunction(a, op, b):
     def toJSVar(input):
         res = resolveIdentifierOrObj(input)
         if res['type'] == 'identifier':
-          return f'state.{res["value"]}'
+          return f'context.{res["value"]}'
         elif res['type'] == 'keyword':
            return res['value']
 
         return res["value"]
 
-    return f'(state) => {toJSVar(a)} {op} {toJSVar(b)}'
+    return f'(context) => {toJSVar(a)} {op} {toJSVar(b)}'
 
 stories = [
   Story(f'(.+) not equal (.+)', lambda config, match, a, b: { 'fn': genCompareFunction(a, '!==', b) }),
