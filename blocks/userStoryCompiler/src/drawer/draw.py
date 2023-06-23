dd4dea8198a9787c24aad188487722ceeeea95a5
Quinten Coltof <quintencoltof1@gmail.com>
2023-06-23 12:33:24 +0000

    Feat(drawing): Draw complete and partial architectures properly Fix(drawing): Allow yaml and JSON based inputs.

--- blocks/userStoryCompiler/src/drawer/draw.py
+++ blocks/userStoryCompiler/src/drawer/draw.py
@@ -7,20 +7,53 @@ def read():
   with open(inFile, 'r') as file:
     doc = yaml.safe_load(file)
 
-  if 'pipelines' not in doc:
+  if 'environments' not in doc:
+    print('Error: No environments found in file', file=sys.stderr)
+    exit(1)
+
+  pipelines = {}
+  for envName in doc['environments']:
+    for pipelineId in doc['environments'][envName]['pipelines']:
+      if pipelineId not in pipelines:
+        pipelines[pipelineId] = {}
+
+      for stepId, step in doc['environments'][envName]['pipelines'][pipelineId]['steps'].items():
+        step['environment'] = envName
+        pipelines[pipelineId][stepId] = step
+
+  print(pipelines)
+  if len(pipelines.keys()) == 0:
     print('Error: No pipelines found in file', file=sys.stderr)
     exit(1)
 
-  return doc['pipelines']
+
+  return pipelines
 
 def genName(stepId, steps):
-  step = steps[str(stepId)]
-  escapedDo = step['do'].replace('"', '\\"')
-  return f'{stepId} - {escapedDo}'
+  step = steps[stepId]
+  if 'do' in step:
+    text = step['do'].replace('"', '\\"')
+  else:
+    text = f"{step['block']} - {step['fn']}"
+  env = f'@{step["environment"]}' if "environment" in step else ''
+  return f'{stepId}{env} - {text}'
+
+def normalizeSteps(steps):
+  """ Needed to normalize stepId type as JSON does not support integer keys in dictionary. """
+  newSteps = {}
+  for stepId, step in steps.items():
+    newSteps[int(stepId)] = step
+
+  return newSteps
 
 def genImage(steps, type='jpg'):
+  steps = normalizeSteps(steps)
   graph = 'digraph G {\n'
   for stepId, step in steps.items():
+    step = steps[stepId]
+    if 'implicitOutStep' in step and step['implicitOutStep'] in steps:
+      step['outStep'] = step['implicitOutStep']
+
     if 'outStep' in step:
       graph += f'"{genName(stepId, steps)}" -> "{genName(step["outStep"], steps)}"\n'
 
@@ -39,6 +72,6 @@ def genImage(steps, type='jpg'):
 if __name__ == '__main__':
   pipelines = read()
 
-  for pid, pipeline in enumerate(pipelines):
+  for pid, pipeline in pipelines.items():
     with open(f'{pid}.jpg', 'wb') as file:
-      file.write(genImage(pipeline['steps']))
+      file.write(genImage(pipeline))
