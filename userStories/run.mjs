a3aa62754c574db520d76ce034c54f0e3970cf1a
Quinten Coltof <quintencoltof1@gmail.com>
2023-06-23 16:54:39 +0000

    Chore(userStories): Update user stories to new format

--- userStories/run.mjs
+++ userStories/run.mjs
@@ -2,13 +2,16 @@ import fs from 'node:fs/promises';
 import { join } from 'node:path';
 import api from '../blocks/architectureManager/frontend/src/api.js'
 
+import { execFile } from 'node:child_process';
+import { promisify } from 'node:util';
+const execFilePromise = promisify(execFile);
+// TODO: Use cli
+
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-async function uploadFile(path) {
-  const yaml = (await fs.readFile(path)).toString();
-  const compiledYaml = (await api.userStories.compile(yaml)).data;
-
-  return await api.architecture.create(compiledYaml);
+async function handleFile(path) {
+  const res = await execFilePromise('python', ['../blocks/userStoryCompiler/src/run.py', path]);
+  await execFilePromise('node', ['../cli/cli.js', './compiled.yml']);
 }
 
 async function main() {
@@ -20,7 +23,8 @@ async function main() {
   for (const file of await fs.readdir(root, { withFileTypes: true })) {
     if (!file.isFile()) { continue; }
     const path = join(root, file.name);
-    console.log(await uploadFile(path));
+    await handleFile(path);
+    console.log('Deployed', file.name)
   }
 }
 
